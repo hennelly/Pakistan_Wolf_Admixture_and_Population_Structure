023fc0c41e8cdc5f84fe6976e214daeb1ad6d7f4
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-06-26 21:56:25 -0700

    Update 09_GBInmport.sh

--- 01_Alignment_SNP_Calling/09_GBInmport.sh
+++ 01_Alignment_SNP_Calling/09_GBInmport.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -l
 #SBATCH --job-name=gbdb
-#SBATCH --array=1-40%30
+#SBATCH --array=1-78%40
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --time 3-00:00:00
 #SBATCH --mem=20GB
-#SBATCH -p bmm
-#SBATCH -A ctbrowngrp
-#SBATCH -o /home/hennelly/projects/GATK/slurmout/%A_%a.out
-#SBATCH -e /home/hennelly/projects/GATK/slurmout/%A_%a.err
+#SBATCH -p high
+#SBATCH -o /home/hennelly/Chapter3/Alignment/slurmout/GenomeDBImport_%A_%a.out
+#SBATCH -e /home/hennelly/Chapter3/Alignment/slurmoutGenomeDBImport_%A_%a.err
 
 module load R
 module load maven
 module load java
-module load GATK
+
+export PATH=$PATH:/home/hennelly/Chapter3/bin/gatk-4.2.6.0
 
 CHR=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/hennelly/projects/GATK/scripts/chromosome_lengths.txt | cut -f1)
-SAMPLEMAP=/home/hennelly/projects/GATK/scripts/mygcflist.txt
-OUTDIR=/home/hennelly/projects/GATK/GenomeDBImport/
-TEMPDIR=/home/hennelly/projects/GATK/scratch/
+SAMPLEMAP=/home/hennelly/Chapter3/Alignment/scripts/myGVCFlist_tab.txt
+OUTDIR=/home/hennelly/Chapter3/Alignment/GenomeDBImport/
+TEMPDIR=/home/hennelly/Chapter3/Alignment/scratch_GBDImport/
 
 gatk --java-options "-Xmx20g -Xms20g" \
     GenomicsDBImport \
