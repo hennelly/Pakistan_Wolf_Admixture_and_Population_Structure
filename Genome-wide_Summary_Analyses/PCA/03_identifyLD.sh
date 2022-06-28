aa1e17dfd8cd244e82cb5e156b4a8217b190301d
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-06-27 19:32:36 -0700

    Create 03_identifyLD.sh

---
+++ Genome-wide_Summary_Analyses/PCA/03_identifyLD.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --job-name=pruneLD
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --time 3-10:00:00
+#SBATCH --mem=8GB
+#SBATCH -p high
+#SBATCH -o /home/hennelly/Chapter3/PCA/slurmout/pruneLD_onlygraywolf.out
+#SBATCH -e /home/hennelly/Chapter3/PCA/slurmout/pruneLD_onlygraywolf.err
+
+module load plink 
+
+FILE=/home/hennelly/Chapter3/FinalDataset/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_noastricks_adjusted.recode.vcf
+OUTFILE=/home/hennelly/Chapter3/FinalDataset/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_noastricks_adjusted_pruneLD0.5
+
+plink --vcf ${FILE} \
+--const-fid 0 \
+--dog \
+--allow-extra-chr \
+--indep-pairwise 50 10 0.5 \
+--geno 0.1 \
+--out ${OUTFILE}
