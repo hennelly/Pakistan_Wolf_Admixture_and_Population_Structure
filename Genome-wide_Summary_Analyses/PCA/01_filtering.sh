702bf5771169a04d768d62f93d45a046edc4f991
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-06-27 17:33:31 -0700

    Create 01_filtering.sh

---
+++ Genome-wide_Summary_Analyses/PCA/01_filtering.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --job-name=filtering_PCA
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --time 3-10:00:00
+#SBATCH --mem=8GB
+#SBATCH -p high
+#SBATCH -o /home/hennelly/Chapter3/PCA/slurmout/filtering_allsamples.out
+#SBATCH -e /home/hennelly/Chapter3/PCA/slurmout/filtering_allsamples.err
+
+FILE=/home/hennelly/Chapter3/FinalDataset/vcffiles/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_mac3.recode.vcf
+OUTFILE=/home/hennelly/Chapter3/FinalDataset/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_noastricks.recode.vcf
+## Remove astricks
+grep -v "*" ${FILE} > ${OUTFILE}
+
+## fix the variant ID column
+FINALFILE=/home/hennelly/Chapter3/FinalDataset/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_noastricks_adjusted.recode.vcf
+awk 'BEGIN{OFS="\t"} !/#/ {sub(/\./, $1"_"$2, $3)}1' ${OUTFILE} > ${FINALFILE}
