6679f872e56dc0c15a3097351fc58533d9e05e77
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-06-27 19:40:41 -0700

    Update 02_pca.sh

--- Genome-wide_Summary_Analyses/PCA/02_pca.sh
+++ Genome-wide_Summary_Analyses/PCA/02_pca.sh
@@ -10,9 +10,9 @@
 
 module load plink
 
-FILE=/home/hennelly/Chapter3/FinalDataset/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_noastricks_adjusted.recode.vcf
-OUTFILE=/home/hennelly/Chapter3/PCA/results/PCAresults_onlygraywolf
-plink --vcf ${FILE} \
+FILE=/home/hennelly/Chapter3/FinalDataset/PCAdataset/Autosomes_gatkfilter_noindels_minQ30_onlywolves_mac3_noastricks_adjusted_pruneLD0.5_geno0.1
+OUTFILE=/home/hennelly/Chapter3/PCA/results/PCAresults_onlygraywolf_pruneLD
+plink --file ${FILE} \
 --pca var-wts \
 --const-fid 0 \
 --dog \
