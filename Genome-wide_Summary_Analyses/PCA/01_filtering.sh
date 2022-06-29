11b88a219ce8c42d9bceba0fea89bb7ac9a40487
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-06-28 21:43:09 -0700

    Update 01_filtering.sh

--- Genome-wide_Summary_Analyses/PCA/01_filtering.sh
+++ Genome-wide_Summary_Analyses/PCA/01_filtering.sh
@@ -8,11 +8,11 @@
 #SBATCH -o /home/hennelly/Chapter3/PCA/slurmout/filtering_allsamples.out
 #SBATCH -e /home/hennelly/Chapter3/PCA/slurmout/filtering_allsamples.err
 
-FILE=/home/hennelly/Chapter3/FinalDataset/vcffiles/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_mac3.recode.vcf
-OUTFILE=/home/hennelly/Chapter3/FinalDataset/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_noastricks.recode.vcf
+FILE=/home/hennelly/Chapter3/PCA/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_mac3.recode.vcf
+OUTFILE=/home/hennelly/Chapter3/PCA/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_mac3_noastrick.recode.vcf
 ## Remove astricks
 grep -v "*" ${FILE} > ${OUTFILE}
 
 ## fix the variant ID column
-FINALFILE=/home/hennelly/Chapter3/FinalDataset/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_noastricks_adjusted.recode.vcf
+FINALFILE=/home/hennelly/Chapter3/PCA/PCAdataset/Autosomes_onlywolves_gatkfilter_noindels_minQ30_maxmissing0.9_mac3_noastrick_adjusted.recode.vcf
 awk 'BEGIN{OFS="\t"} !/#/ {sub(/\./, $1"_"$2, $3)}1' ${OUTFILE} > ${FINALFILE}
