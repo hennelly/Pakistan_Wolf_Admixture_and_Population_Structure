742dac928732217554ef41e0e3f11f6857ba6b39
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-06-30 12:17:49 -0700

    Create 01_NGSadmix_K2.sh

---
+++ Genome-wide_Summary_Analyses/NGSadmix/01_NGSadmix_K2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --job-name=NGSadmix_allchr_prune50_K2_2
+#SBATCH --nodes 1
+#SBATCH --ntasks 2
+#SBATCH --time 5:00:00
+#SBATCH --mem-per-cpu=30GB
+#SBATCH -p bmh
+#SBATCH -A ctbrowngrp
+#SBATCH -o /home/sophiepq/GrayFoxWGS/GrayFox1/slurmlogs/NGSadmix_allchr_prune50_K2_2.out
+#SBATCH -e /home/sophiepq/GrayFoxWGS/GrayFox1/slurmlogs/NGSadmix_allchr_prune50_K2_2.err
+
+~/bin/NGSadmix -likes ~/GrayFoxWGS/GrayFox1/angsd/vcf/bychr/allchrs_header_prune50.beagle.gz -K 2 -o ~/GrayFoxWGS/GrayFox1/angsd/NGSadmix/NGSadmix_allchr_prune50_K2_2 -P 10
+
+/group/ctbrowngrp2/sophiepq/GrayFoxWGS/GrayFox1/slurmscripts
