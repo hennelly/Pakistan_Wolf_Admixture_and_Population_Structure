49e7d30a27f60df84a7f8fe9a449fe3c7f4b14a8
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-07-03 08:36:26 -0700

    Update 01_NGSadmix_K2.sh

--- 03_Admixture_Analysis/01_NGSadmix_K2.sh
+++ 03_Admixture_Analysis/01_NGSadmix_K2.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks 2
 #SBATCH --time 5:00:00
 #SBATCH --mem-per-cpu=30GB
-#SBATCH -p hennelly
-#SBATCH -o /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K2.out
-#SBATCH -e /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K2.err
+#SBATCH -p high
+#SBATCH -o /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K2_Shimshal.out
+#SBATCH -e /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K2_Shimshal.err
 
-/home/sophiepq/bin/NGSadmix -likes ~/Chapter3/angsdPCA/beagle_autosomes/angsd_autosomes_output.beagle.gz -K 2 -o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_K2 -P 10
+/home/sophiepq/bin/NGSadmix -likes ~/Chapter3/ANGSD/beagle_autosomes/angsd_autosomes_Eurasianwolves_output.beagle.gz -K 2 -o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_noShimshal_K2 -P 10
 
