7ea2480e43efe6860662446e6d0c67e58f50dbaa
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-07-02 18:54:30 -0700

    Create 01_PCAngsd.sh

---
+++ 02_PCAngsd/01_PCAngsd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --job-name=ang_pca
+#SBATCH --nodes 1
+#SBATCH --ntasks 2
+#SBATCH --time 3-24:00:00
+#SBATCH --mem-per-cpu=10GB
+#SBATCH -p high
+#SBATCH -o /home/hennelly/Chapter3/angsdPCA/slurmout/angsdPCA_try2.out
+#SBATCH -e /home/hennelly/Chapter3/angsdPCA/slurmout/angsdPCA_try2.err
+
+
+REF=/home/hennelly/fastqfiles/DogRefwithY/genomes/canFam3_withY.fa
+LIST=/home/hennelly/Chapter3/angsdPCA/scripts/bam_autosomes.txt
+OUT=/home/hennelly/Chapter3/angsdPCA/beagle_autosomes
+
+module load angsd
+
+angsd -out ${OUT}/angsd_autosomes_output -bam ${LIST} -ref ${REF} -nThreads 2 \
+    -GL 1 \
+    -trim 0 -C 50 -remove_bads 0 -uniqueOnly 1 \
+    -minMapQ 20 -minQ 20 -minInd 41 \
+    -doGlf 2 -doMajorMinor 1 -doMaf 1 \
+    -SNP_pval 1e-6
