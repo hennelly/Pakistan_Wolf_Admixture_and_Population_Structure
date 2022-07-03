2e259c04a7de691b443a43be3ed29a20c981e04c
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-07-03 10:35:05 -0700

    Update 08_NGSadmix_K4_allcanids.sh

--- 03_Admixture_Analysis/08_NGSadmix_K4_allcanids.sh
+++ 03_Admixture_Analysis/08_NGSadmix_K4_allcanids.sh
@@ -5,7 +5,7 @@
 #SBATCH --time 5:00:00
 #SBATCH --mem-per-cpu=30GB
 #SBATCH -p high
-#SBATCH -o /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K3_allcanids.out
-#SBATCH -e /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K3_allcanids.err
+#SBATCH -o /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K4_allcanids.out
+#SBATCH -e /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K4_allcanids.err
 
-/home/sophiepq/bin/NGSadmix -likes ~/Chapter3/ANGSD/beagle_autosomes/angsd_autosomes_wolvesdogsothercanids_output.beagle.gz -K 3 -o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_allcanids_K3 -P 10
+/home/sophiepq/bin/NGSadmix -likes ~/Chapter3/ANGSD/beagle_autosomes/angsd_autosomes_wolvesdogsothercanids_output.beagle.gz -K 4 -o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_allcanids_K4 -P 10
