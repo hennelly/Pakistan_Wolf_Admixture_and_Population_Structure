1369f512d784d1409f744497a579204260ed402d
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-07-03 08:33:45 -0700

    Update 01_PCAngsd.sh

--- 02_PCAngsd/01_PCAngsd.sh
+++ 02_PCAngsd/01_PCAngsd.sh
@@ -5,14 +5,14 @@
 #SBATCH --time 5:00:00
 #SBATCH --mem-per-cpu=60GB
 #SBATCH -p high
-#SBATCH -o /home/hennelly/Chapter3/angsdPCA/slurmout/pcangsd.out
-#SBATCH -e /home/hennelly/Chapter3/angsdPCA/slurmout/pcangsd.err
+#SBATCH -o /home/hennelly/Chapter3/angsdPCA/slurmout/pcangsd_noshimshal.out
+#SBATCH -e /home/hennelly/Chapter3/angsdPCA/slurmout/pcangsd_noshimshal.err
 
 PCANGSD=/home/cbquinn/bin/pcangsd/pcangsd.py
-IN=~/Chapter3/angsdPCA/results.beagle.gz
+IN=~/Chapter3/ANGSD/beagle_autosomes/angsd_autosomes_Eurasianwolves_output.beagle.gz
 OUTDIR=~/Chapter3/angsdPCA/pca_outputs
 
 module load python
 # Estimate covariance matrix for pca
-python ${PCANGSD} -beagle ${IN} -o ${OUTDIR}/Autosomes_pca -threads 2
+python ${PCANGSD} -beagle ${IN} -o ${OUTDIR}/Autosomes_pca_noShimshal -threads 2
 
