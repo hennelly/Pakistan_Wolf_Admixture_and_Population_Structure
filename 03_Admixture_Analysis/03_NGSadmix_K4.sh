fcf586a9152295c1910ee4500ccd70cf7849c8a1
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-07-03 10:21:28 -0700

    Update 03_NGSadmix_K4.sh

--- 03_Admixture_Analysis/03_NGSadmix_K4.sh
+++ 03_Admixture_Analysis/03_NGSadmix_K4.sh
@@ -5,7 +5,8 @@
 #SBATCH --time 5:00:00
 #SBATCH --mem-per-cpu=30GB
 #SBATCH -p high
-#SBATCH -o /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K4.out
-#SBATCH -e /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K4.err
+#SBATCH -o /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K4_noShimshal.out
+#SBATCH -e /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K4_noShimshal.err
 
-/home/sophiepq/bin/NGSadmix -likes ~/Chapter3/angsdPCA/beagle_autosomes/angsd_autosomes_output.beagle.gz -K 4 -o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_K4 -P 10
+/home/sophiepq/bin/NGSadmix -likes ~/Chapter3/ANGSD/beagle_autosomes/angsd_autosomes_Eurasianwolves_output.beagle.gz -K 4 -o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_K4_noShimshal -P 10
+
