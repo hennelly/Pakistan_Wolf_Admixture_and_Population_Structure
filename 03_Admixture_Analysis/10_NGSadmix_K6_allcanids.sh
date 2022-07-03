5ab5c7e1c85734fb3b08acb74bbc2edc7a4b9c4e
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-07-03 10:36:40 -0700

    Update 10_NGSadmix_K6_allcanids.sh

--- 03_Admixture_Analysis/10_NGSadmix_K6_allcanids.sh
+++ 03_Admixture_Analysis/10_NGSadmix_K6_allcanids.sh
@@ -8,4 +8,4 @@
 #SBATCH -o /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K6_allcanids.out
 #SBATCH -e /home/hennelly/Chapter3/NGSadmix/slurmout/NGSadmix_allchr_K6_allcanids.err
 
-/home/sophiepq/bin/NGSadmix -likes ~/Chapter3/ANGSD/beagle_autosomes/angsd_autosomes_wolvesdogsothercanids_output.beagle.gz -K 6 -o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_allcanids_o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_allcanidsK6 -P 10
+/home/sophiepq/bin/NGSadmix -likes ~/Chapter3/ANGSD/beagle_autosomes/angsd_autosomes_wolvesdogsothercanids_output.beagle.gz -K 6 -o ~/Chapter3/NGSadmix/results/NGSadmix_allchr_allcanids_K6 -P 10
