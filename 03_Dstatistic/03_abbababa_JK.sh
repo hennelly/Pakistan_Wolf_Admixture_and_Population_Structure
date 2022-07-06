a343c58a0f94d20ea66134ead0baf6ae2c7351f3
Lauren Hennelly <lmhennelly@ucdavis.edu>
2022-07-06 08:09:30 -0700

    Update 03_abbababa_JK.sh

--- 03_Dstatistic/03_abbababa_JK.sh
+++ 03_Dstatistic/03_abbababa_JK.sh
@@ -6,10 +6,10 @@
 #SBATCH --mem-per-cpu=500M
 #SBATCH -p bmh
 #SBATCH -A ctbrowngrp
-#SBATCH -o /home/hennelly/Chapter3/Dstatistic/slurmout/angsd_JK.out
-#SBATCH -e /home/hennelly/Chapter3/Dstatistic/slurmout/angsd_JK.err
+#SBATCH -o /home/hennelly/Chapter3/Dstatistic/slurmout/angsd_JK_minQ30.out
+#SBATCH -e /home/hennelly/Chapter3/Dstatistic/slurmout/angsd_JK_minQ30.err
 
 module load angsd
 
-Rscript /share/apps/angsd-0.934/R/jackKnife.R file=/home/hennelly/Chapter3/Dstatistic/results/abba_July32022.abbababa indNames=/home/hennelly/Chapter3/angsdPCA/scripts/bam_autosomes_noShimshal.txt outfile=abba_July5th2022.abbaJKout
+Rscript /share/apps/angsd-0.934/R/jackKnife.R file=/home/hennelly/Chapter3/Dstatistic/results/abba_July52022_minQ30.abbababa indNames=/home/hennelly/Chapter3/angsdPCA/scripts/bam_autosomes_noShimshal.txt outfile=abba_July5th2022_minQ30.abbaJKout
 
